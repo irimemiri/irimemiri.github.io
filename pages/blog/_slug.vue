c809e1958f4db657a9eba76211a4ce7f333f8fca
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-17 01:38:13 +0900

    記事一覧と詳細ページの表示まで

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>{{blog.title}}</h1>
+    <dl>
+      <dt>date</dt>
+      <dd>{{blog.date}}</dd>
+    </dl>
+    <div><img :src="blog.image" /></div>
+    <div v-for="tag in blog.tags" :key="tag">{{tag}}</div>
+
+    <nuxt-content :document="blog" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const blog = await $content('blog', params.slug || 'index').fetch()
+    return { blog }
+  }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="n in blog" :key="n.slug">
+      <nuxt-link :to="'/blog/'+ n.slug">{{n.title}} {{n.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  async asyncData ({ $content, params }) {
+    const query = await $content('blog' || 'index').limit(15)
+    const blog = await query.fetch()
+    return { blog }
+  }
+}
+</script>
