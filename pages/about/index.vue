871159f9bf58bc3c10440bf9b9dc890f63400e00
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-05-26 00:25:40 +0900

    noindex nofollowにする

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,70 +1,70 @@
 <template>
   <div class="profile-wrapper">
     <h1 class="page-title"><span class="marker">私について</span></h1>
-    <!-- <div class="profile-wrapper"> -->
-    <section class="sect-wrapper">
-      <h2 class="sect-title">プロフィール</h2>
-      <div class="sect-content">
-        <p class="my-name">伊東絵美莉(@irimemiri)</p>
-        <p class="my-comment">1995年生まれ。大学在学中に大学生協の企画広報っぽいことをしてPRや広告に興味を持ち、卒業後は広告系の会社にWebエンジニアとして就職。
-          2020年、退職をきっかけにデザインに興味を持ち、職業訓練校でWeb制作を3ヶ月間学びました。
-          何事も考え抜くタイプで、目的に合わせて色々な手段を試すことが好きです。趣味は絵を描くこと、音楽鑑賞、語学。</p>
-      </div>
-    </section>
-    <section class="sect-wrapper">
-      <h2 class="sect-title">バックグラウンド</h2>
-      <div class="sect-content">
-        <dl class="history">
-          <div class="history-row">
-            <dt class="period">2017</dt>
-            <dd class="event">大学の英文学科を卒業</dd>
-          </div>
-          <div class="history-row">
-            <dt class="period">2017-2019</dt>
-            <dd class="event">DM発送代行会社の社内SE</dd>
-          </div>
-          <div class="history-row">
-            <dt class="period">2019-2020</dt>
-            <dd class="event">インターネット広告代理店のWebエンジニア</dd>
-          </div>
-          <div class="history-row">
-            <dt class="period">2020-2021</dt>
-            <dd class="event">職業訓練校でデザインを学ぶ</dd>
-          </div>
-          <div class="history-row">
-            <dt class="period">2021-現在</dt>
-            <dd class="event">デザイナーを目指して転職活動中</dd>
-          </div>
-        </dl>
-      </div>
-    </section>
-    <section class="sect-wrapper">
-      <h2 class="sect-title">スキルセット</h2>
-      <div class="sect-content">
-        <dl class="skillset">
-          <div class="skillset-row">
-            <dt class="skill-type">Backend</dt>
-            <dd class="skill-text">
-              Ruby/Ruby on Rails/PHP/CodeIgniter/Node.js
-            </dd>
-          </div>
-          <div class="skillset-row">
-            <dt class="skill-type">Frontend</dt>
-            <dd class="skill-text">
-              HTML5/CSS3/JavaScript/vue.js/jQuery
-            </dd>
-          </div>
-          <div class="skillset-row">
-            <dt class="skill-type">Design Tools</dt>
-            <dd class="skill-text">
-              Sketch/Illustrator/Photoshop
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </section>
+    <div class="profile-inner-box">
+      <section class="sect-wrapper">
+        <h2 class="sect-title">プロフィール</h2>
+        <div class="sect-content">
+          <p class="my-name">伊東絵美莉(@irimemiri)</p>
+          <p class="my-comment">1995年生まれ。大学在学中に大学生協の企画広報っぽいことをしてPRや広告に興味を持ち、卒業後は広告系の会社にWebエンジニアとして就職。
+            2020年、退職をきっかけにデザインに興味を持ち、職業訓練校でWeb制作を3ヶ月間学びました。
+            何事も考え抜くタイプで、目的に合わせて色々な手段を試すことが好きです。趣味は絵を描くこと、音楽鑑賞、語学。</p>
+        </div>
+      </section>
+      <section class="sect-wrapper">
+        <h2 class="sect-title">バックグラウンド</h2>
+        <div class="sect-content">
+          <dl class="history">
+            <div class="history-row">
+              <dt class="period">2017</dt>
+              <dd class="event">大学の英文学科を卒業</dd>
+            </div>
+            <div class="history-row">
+              <dt class="period">2017-2019</dt>
+              <dd class="event">DM発送代行会社の社内SE</dd>
+            </div>
+            <div class="history-row">
+              <dt class="period">2019-2020</dt>
+              <dd class="event">インターネット広告代理店のWebエンジニア</dd>
+            </div>
+            <div class="history-row">
+              <dt class="period">2020-2021</dt>
+              <dd class="event">職業訓練校でデザインを学ぶ</dd>
+            </div>
+            <div class="history-row">
+              <dt class="period">2021-現在</dt>
+              <dd class="event">デザイナーを目指して転職活動中</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+      <section class="sect-wrapper">
+        <h2 class="sect-title">スキルセット</h2>
+        <div class="sect-content">
+          <dl class="skillset">
+            <div class="skillset-row">
+              <dt class="skill-type">Backend</dt>
+              <dd class="skill-text">
+                Ruby/Ruby on Rails/PHP/CodeIgniter/Node.js
+              </dd>
+            </div>
+            <div class="skillset-row">
+              <dt class="skill-type">Frontend</dt>
+              <dd class="skill-text">
+                HTML5/CSS3/JavaScript/vue.js/jQuery
+              </dd>
+            </div>
+            <div class="skillset-row">
+              <dt class="skill-type">Design Tools</dt>
+              <dd class="skill-text">
+                Sketch/Illustrator/Photoshop
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </section>
     </div>
-  <!-- </div> -->
+  </div>
 </template>
 
 <style>
@@ -138,6 +138,10 @@
     margin-right: auto;
     padding-top: 150px;
   }
+
+  /* .profile-inner-box { */
+    /* background-color: #fff; */
+  /* } */
 
   .skillset {
     font-size: .9em;
