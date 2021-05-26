f9a61ff070d926e3f5d30616ba1706bcf1020430
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-05-26 23:44:20 +0900

    レスポンシブ対応 プロフィール 結構変えた

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,80 +1,86 @@
 <template>
   <div class="profile-wrapper">
-    <h1 class="page-title"><span class="marker">私について</span></h1>
-    <div class="profile-inner-box">
-      <section class="sect-wrapper">
-        <h2 class="sect-title">プロフィール</h2>
-        <div class="sect-content">
-          <p class="my-name">伊東絵美莉 (<a href="https://github.com/irimemiri" class="github-link">@irimemiri</a>)</p>
-          <p class="my-comment">1995年生まれ。大学在学中に大学生協の企画広報活動をしたことでPRや広告に興味を持ち、卒業後は広告系の会社にWebエンジニアとして就職。
-            2020年、退職をきっかけにデザインに興味を持ち、職業訓練校でWeb制作を3ヶ月間学びました。
-            何事も考え抜くタイプで、目的に合わせて色々な手段を試すことが好きです。趣味は絵を描くこと、音楽鑑賞、語学。</p>
-        </div>
-      </section>
-      <section class="sect-wrapper">
-        <h2 class="sect-title">バックグラウンド</h2>
-        <div class="sect-content">
-          <dl class="history">
-            <div class="history-row">
-              <dt class="period">2017</dt>
-              <dd class="event">大学の英文学科を卒業</dd>
-            </div>
-            <div class="history-row">
-              <dt class="period">2017-2019</dt>
-              <dd class="event">DM発送代行会社の社内SE</dd>
-            </div>
-            <div class="history-row">
-              <dt class="period">2019-2020</dt>
-              <dd class="event">インターネット広告代理店のWebエンジニア</dd>
-            </div>
-            <div class="history-row">
-              <dt class="period">2020-2021</dt>
-              <dd class="event">職業訓練校で3ヶ月間Web制作を学ぶ
-              </dd>
-            </div>
-            <div class="history-row">
-              <dt class="period">2021-現在</dt>
-              <dd class="event">デザイナーを目指して転職活動中</dd>
-            </div>
-          </dl>
-        </div>
-      </section>
-      <section class="sect-wrapper">
-        <h2 class="sect-title">スキルセット</h2>
-        <div class="sect-content">
-          <dl class="skillset">
-            <div class="skillset-row">
-              <dt class="skill-type">Backend</dt>
-              <dd class="skill-text">
-                Ruby/Ruby on Rails/PHP/CodeIgniter/Node.js
-              </dd>
-            </div>
-            <div class="skillset-row">
-              <dt class="skill-type">Frontend</dt>
-              <dd class="skill-text">
-                HTML5/CSS3/JavaScript/vue.js/jQuery
-              </dd>
-            </div>
-            <div class="skillset-row">
-              <dt class="skill-type">Design Tools</dt>
-              <dd class="skill-text">
-                Sketch/Illustrator/Photoshop
-              </dd>
-            </div>
-            <div class="skillset-row">
-              <dt class="skill-type">Other Tools</dt>
-              <dd class="skill-text">
-                WordPress/Google Analytics/AWS
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </section>
-    </div>
+  <h1 class="page-title"><span class="marker">私について</span></h1>
+    <section class="sect-wrapper">
+      <h2 class="sect-title">プロフィール</h2>
+      <div class="sect-content">
+        <p class="my-name">伊東絵美莉 (<a href="https://github.com/irimemiri" class="github-link">@irimemiri</a>)</p>
+        <p class="my-comment">1995年生まれ。大学在学中に大学生協の企画広報活動をしたことでPRや広告に興味を持ち、卒業後は広告系の会社にWebエンジニアとして就職。
+          2020年、退職をきっかけにデザインに興味を持ち、職業訓練校でWeb制作を3ヶ月間学びました。
+          何事も考え抜くタイプで、目的に合わせて色々な手段を試すことが好きです。趣味は絵を描くこと、音楽鑑賞、語学。</p>
+      </div>
+    </section>
+    <section class="sect-wrapper">
+      <h2 class="sect-title">バックグラウンド</h2>
+      <div class="sect-content">
+        <dl class="history">
+          <div class="history-row">
+            <dt class="period">2017</dt>
+            <dd class="event">大学の英文学科を卒業</dd>
+          </div>
+          <div class="history-row">
+            <dt class="period">2017-2019</dt>
+            <dd class="event">DM発送代行会社の社内SE</dd>
+          </div>
+          <div class="history-row">
+            <dt class="period">2019-2020</dt>
+            <dd class="event">インターネット広告代理店のWebエンジニア</dd>
+          </div>
+          <div class="history-row">
+            <dt class="period">2020-2021</dt>
+            <dd class="event">職業訓練校で3ヶ月間Web制作を学ぶ
+            </dd>
+          </div>
+          <div class="history-row">
+            <dt class="period">2021-現在</dt>
+            <dd class="event">デザイナーを目指して転職活動中</dd>
+          </div>
+        </dl>
+      </div>
+    </section>
+    <section class="sect-wrapper">
+      <h2 class="sect-title">スキルセット</h2>
+      <div class="sect-content">
+        <dl class="skillset">
+          <div class="skillset-row">
+            <dt class="skill-type">Backend</dt>
+            <dd class="skill-text">
+              Ruby/Ruby on Rails/PHP/CodeIgniter/Node.js
+            </dd>
+          </div>
+          <div class="skillset-row">
+            <dt class="skill-type">Frontend</dt>
+            <dd class="skill-text">
+              HTML5/CSS3/JavaScript/vue.js/jQuery
+            </dd>
+          </div>
+          <div class="skillset-row">
+            <dt class="skill-type">Design Tools</dt>
+            <dd class="skill-text">
+              Sketch/Illustrator/Photoshop
+            </dd>
+          </div>
+          <div class="skillset-row">
+            <dt class="skill-type">Other Tools</dt>
+            <dd class="skill-text">
+              WordPress/Google Analytics/AWS
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
+.profile-wrapper {
+  width: 76.562%; /* 980/1280 */
+  max-width: 980px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 150px;
+}
+
 .page-title .marker {
   background: linear-gradient(transparent 60%, #c1e0ff 60%);
 }
@@ -84,21 +90,23 @@
 }
 
 .sect-wrapper {
-  width: 780px;
+  width: 79.591%; /* 780/980 */
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
   margin-bottom: 100px;
 }
 
 .sect-wrapper .sect-title {
-  width: 180px;
+  width: 23.076; /* 180/780 */
   font-size: 1em;
   text-align: right;
 }
 
 .sect-wrapper .sect-content {
-  width: 580px;
+  width: 74.358%; /* 580/780 */
   line-height: 1.8;
 }
 
@@ -136,32 +144,20 @@
 .history .history-row, .skillset-row {
   display: flex;
   flex-wrap: wrap;
-  width: 680px;
+  justify-content: space-between;
+  width: 100%; /* 580/580 */
   line-height: 2em;
 }
 
 .history .history-row .period, .skillset-row .skill-type {
-  width: 180px;
+  width: 31.034%; /* 180/580 */
   letter-spacing: .1em;
   text-align: right;
-  padding-right: 20px;
 }
 .history .history-row .event, .skillset-row .skill-text {
-  width: 480px;
+  width: 65.517%; /* 380/580 */
   letter-spacing: .1em;
 }
-
-/* とりあえず全体に幅設定 */
-.profile-wrapper {
-  width: 980px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 150px;
-}
-
-/* .profile-inner-box { */
-  /* background-color: #fff; */
-/* } */
 
 /*
   ===========================
@@ -173,7 +169,7 @@
 @media screen and (max-width: 780px) {
   /* TODO: 各ページ統一 */
   .profile-wrapper {
-    width: 370px;
+   width: 96%; /* 370/375 edited */
     padding-top: 60px;
   }
 
@@ -182,19 +178,23 @@
   }
 
   .sect-wrapper {
-    width: 370px;
+    width: 100%;
   }
 
   .sect-wrapper .sect-title {
-    width: 370px;
+    width: 100%;
     font-size: 1.2em;
     font-weight: bold;
     text-align: center;
     margin-bottom: 20px;
   }
 
+  .my-name {
+    font-size: .9em;
+  }
+
   .sect-wrapper .sect-content {
-    width: 370px;
+    width: 100%;
   }
 
   .history, .skillset {
@@ -202,15 +202,15 @@
   }
 
   .history .history-row, .skillset-row {
-    width: 370px;
+    width: 100%;
   }
 
   .history .history-row .period, .skillset-row .skill-type {
-    width: 140px;
+    width: 37.837%; /* 140/370 */
   }
 
   .history .history-row .event, .skillset-row .skill-text {
-    width: 215px;
+    width: 58.108%; /* 215/370 */
   }
 }
 </style>
